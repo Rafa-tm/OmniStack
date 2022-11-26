cfb299811f909a04f5c71b638be17481d1af7b1a
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2022-11-26 18:48:14 -0300

    test: testes de integração realizados

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const {errors} = require('celebrate')
 const routes = require('./routes');
 
 const app = express();
@@ -13,8 +14,9 @@ app.use(cors({
 */
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
 
 
 /*
